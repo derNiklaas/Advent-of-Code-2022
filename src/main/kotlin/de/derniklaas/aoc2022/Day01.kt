c0a4a4c7e39e5c7581e1880b2251d423edd5e68c
Niklas <derNiklaas@users.noreply.github.com>
2022-12-01 07:42:02 +0100

    Day01: Cleanup and simplification

--- src/main/kotlin/de/derniklaas/aoc2022/Day01.kt
+++ src/main/kotlin/de/derniklaas/aoc2022/Day01.kt
@@ -5,14 +5,16 @@ fun main() {
     Day01(Basket.fromInput(input)).execute()
 }
 
-public class Day01(private val input: List<Basket>) : Day {
+public class Day01(input: List<Basket>) : Day {
+
+    private val sorted = input.sortedByDescending { it.calories }
+
     override fun part1(): Int {
-        return input.maxOf { it.calories }
+        return sorted.first().calories
     }
 
     override fun part2(): Int {
-        val sorted = input.sortedByDescending { it.calories }
-        return sorted[0].calories + sorted[1].calories + sorted[2].calories
+        return sorted.subList(0, 3).sumOf { it.calories }
     }
 }
 
@@ -20,17 +22,17 @@ data class Basket(val calories: Int) {
     companion object {
         public fun fromInput(input: List<String>): List<Basket> {
             val baskets = ArrayList<Basket>()
-            var sum = 0;
+            var sum = 0
             for (line in input) {
                 if (line.isEmpty()) {
                     baskets += Basket(sum)
-                    sum = 0;
+                    sum = 0
                 } else {
-                    sum += line.toInt();
+                    sum += line.toInt()
                 }
             }
             baskets += Basket(sum)
-            return baskets;
+            return baskets
         }
     }
 }
