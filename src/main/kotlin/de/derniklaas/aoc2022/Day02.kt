55c4c2136bc0c3735561f74fca06c98574ab24a0
Niklas <derNiklaas@users.noreply.github.com>
2022-12-02 21:49:55 +0100

    Day2: Simplify code

--- src/main/kotlin/de/derniklaas/aoc2022/Day02.kt
+++ src/main/kotlin/de/derniklaas/aoc2022/Day02.kt
@@ -6,92 +6,40 @@ fun main() {
 }
 
 class Day02(private val input: List<Match>) : Day {
-    override fun part1(): Int {
-        return input.sumOf {
-            it.player2.points + it.getResult()
-        }
-    }
+    override fun part1(): Int = input.sumOf { it.getScore() }
 
-    override fun part2(): Int {
-        return input.map { it.fixMatch() }.sumOf {
-            it.player2.points + it.getResult()
-        }
-    }
+    override fun part2(): Int = input.map { it.fixMatch() }.sumOf { it.getScore() }
 }
 
-class Match(private val player1: RPSType, val player2: RPSType) {
+class Match(private val player1: RPSType, private val player2: RPSType) {
     companion object {
         fun fromInput(input: List<String>): List<Match> {
             return input.map {
-                it.split(" ").map { type ->
-                    when (type) {
-                        "A" -> RPSType.ROCK
-                        "B" -> RPSType.PAPER
-                        "C" -> RPSType.SCISSORS
-                        "X" -> RPSType.X
-                        "Y" -> RPSType.Y
-                        "Z" -> RPSType.Z
-                        else -> {
-                            error("Invalid state")
-                        }
-                    }
-                }
-            }.map {
-                val (first, second) = it
-                Match(first, second)
-            }
+                it.split(" ").map { type -> type.toRPSType() }
+            }.map { Match(it.first(), it.last()) }
         }
     }
 
-    fun getResult(): Int {
-        return if (player1.getLosingType(player1) == player2.getNormalType(player2)) 6
-        else if (player1.getWinningType(player1) == player2.getNormalType(player2)) 0
-        else 3
+    fun getScore() = player2.points + getResult()
+
+    private fun getResult(): Int {
+        return if (player1.getStrongType() == player2.getNormalType()) 6
+        else if (player1.getNormalType() == player2.getNormalType()) 3
+        else 0
     }
 
     fun fixMatch(): Match {
         val first = player1
         val second = when (player2) {
-            RPSType.X -> {
-                when (first) {
-                    RPSType.ROCK -> RPSType.SCISSORS
-                    RPSType.SCISSORS -> RPSType.PAPER
-                    RPSType.PAPER -> RPSType.ROCK
-                    else -> {
-                        error("Invalid state")
-                    }
-                }
-            }
-
-            RPSType.Y -> {
-                when (first) {
-                    RPSType.ROCK -> RPSType.ROCK
-                    RPSType.SCISSORS -> RPSType.SCISSORS
-                    RPSType.PAPER -> RPSType.PAPER
-                    else -> {
-                        error("Invalid state")
-                    }
-                }
-            }
-
-            RPSType.Z -> {
-                when (first) {
-                    RPSType.ROCK -> RPSType.PAPER
-                    RPSType.SCISSORS -> RPSType.ROCK
-                    RPSType.PAPER -> RPSType.SCISSORS
-                    else -> {
-                        error("Invalid state")
-                    }
-                }
-            }
-
+            RPSType.X -> first.getWeakType()
+            RPSType.Y -> first
+            RPSType.Z -> first.getStrongType()
             else -> {
                 error("Invalid state")
             }
         }
         return Match(first, second)
     }
-
 }
 
 enum class RPSType(val points: Int) {
@@ -102,30 +50,34 @@ enum class RPSType(val points: Int) {
     Y(2),
     Z(3);
 
-    fun getNormalType(type: RPSType): RPSType = when (type) {
-        ROCK -> ROCK
-        PAPER -> PAPER
-        SCISSORS -> SCISSORS
+    fun getNormalType(): RPSType = when (this) {
+        ROCK, PAPER, SCISSORS -> this
         X -> ROCK
         Y -> PAPER
         Z -> SCISSORS
     }
 
-    fun getLosingType(type: RPSType) = when (type) {
-        ROCK -> PAPER
-        PAPER -> SCISSORS
-        SCISSORS -> ROCK
-        X -> PAPER
-        Y -> SCISSORS
-        Z -> ROCK
+    fun getStrongType() = when (this) {
+        ROCK, X -> PAPER
+        PAPER, Y -> SCISSORS
+        SCISSORS, Z -> ROCK
     }
 
-    fun getWinningType(type: RPSType) = when (type) {
-        ROCK -> SCISSORS
-        PAPER -> ROCK
-        SCISSORS -> PAPER
-        X -> SCISSORS
-        Y -> ROCK
-        Z -> PAPER
+    fun getWeakType() = when (this) {
+        ROCK, X -> SCISSORS
+        PAPER, Y -> ROCK
+        SCISSORS, Z -> PAPER
+    }
+}
+
+private fun String.toRPSType() = when (this) {
+    "A" -> RPSType.ROCK
+    "B" -> RPSType.PAPER
+    "C" -> RPSType.SCISSORS
+    "X" -> RPSType.X
+    "Y" -> RPSType.Y
+    "Z" -> RPSType.Z
+    else -> {
+        error("Invalid state")
     }
 }
