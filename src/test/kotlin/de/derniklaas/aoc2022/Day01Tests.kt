77d4f55b44598c47141a0c56d172d88538447250
Niklas <derNiklaas@users.noreply.github.com>
2022-12-02 15:11:37 +0100

    Day01 Test: Reduce runtime

--- src/test/kotlin/de/derniklaas/aoc2022/Day01Tests.kt
+++ src/test/kotlin/de/derniklaas/aoc2022/Day01Tests.kt
@@ -4,16 +4,15 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class Day01Tests {
+    private val day01 = Day01(Basket.fromInput(readFile("Day01", true)))
 
     @Test
     public fun part1() {
-        val input = readFile("Day01", true)
-        Assertions.assertEquals(24000, Day01(Basket.fromInput(input)).part1())
+        Assertions.assertEquals(24000, day01.part1())
     }
 
     @Test
     public fun part2() {
-        val input = readFile("Day01", true)
-        Assertions.assertEquals(45000, Day01(Basket.fromInput(input)).part2())
+        Assertions.assertEquals(45000, day01.part2())
     }
 }
