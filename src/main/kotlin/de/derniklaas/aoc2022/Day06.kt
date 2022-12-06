16e0ab3e0aa871132edb072d9d7cfad8c2c2d198
Niklas <derNiklaas@users.noreply.github.com>
2022-12-06 19:09:40 +0100

    Day06: Improve Runtime

--- src/main/kotlin/de/derniklaas/aoc2022/Day06.kt
+++ src/main/kotlin/de/derniklaas/aoc2022/Day06.kt
@@ -6,28 +6,18 @@ fun main() {
 }
 
 class Day06(private val input: String) : Day {
-    override fun part1(): Any {
-        for (i in 3 until input.length) {
-            if (buildSet(i, 4).size == 4) {
-                return i + 1
-            }
-        }
-        error("No marker found")
+    val set = hashSetOf<Char>()
+    override fun part1(): Int {
+        return input.windowedSequence(4).indexOfFirst {
+            set.clear()
+            it.all { char -> set.add(char) }
+        } + 4
     }
 
-    override fun part2(): Any {
-        for (i in 13 until input.length) {
-            if (buildSet(i, 14).size == 14) {
-                return i + 1
-            }
-        }
-        error("No marker found")
+    override fun part2(): Int {
+        return input.windowedSequence(14).indexOfFirst {
+            set.clear()
+            it.all { char -> set.add(char) }
+        } + 14
     }
-
-    private fun buildSet(index: Int, length: Int): Set<Char> =
-        buildSet {
-            for (j in 0 until length) {
-                add(input[index - j])
-            }
-        }
 }
